81d75d7cfbc0be386088ad29d284d728a7d6adc3
Hippopoto <40579599+Hippopoto@users.noreply.github.com>
2021-04-18 19:02:24 +0200

    Add files via upload

---
+++ App4/App.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System.IO;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace App4
+{
+    public partial class App : Application
+    {
+        static SQLiteHelper db;
+        
+        public App()
+        {
+           
+            InitializeComponent();
+            
+            MainPage = new MainPage();
+        }
+
+        public static SQLiteHelper SQLiteDatabase
+        {
+            get
+            {
+                if (db == null)
+                {
+                    db = new SQLiteHelper(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "magBrzyski2.db3"));
+                }
+                return db;
+            }
+        }
+      //  void kkk() { MainPage = new MainPage(); }
+        protected override void OnStart()
+        {
+            // ThreadStart ts = new ThreadStart(kkk);
+
+            // Thread pochodnyWatek = new Thread(ts);
+            // Uruchamiamy wątek pochodny
+            //pochodnyWatek.Start();
+            //MainPage = new MainPage();
+
+            
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ App4/MainPage.xaml.cs
@@ -0,0 +1,79 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace App4
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    //[DesignTimeVisible(false)]
+    
+    public partial class MainPage : ContentPage
+    {
+        
+        
+        public MainPage()
+        {
+            InitializeComponent();
+            
+        }
+
+        async private void btnAdd_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new Page1());
+            
+        }
+    /*    async void MyMainThreadCode()
+        {
+            // Code to run on the main thread
+            await Navigation.PushModalAsync(new Page2());
+        }
+        async void Threadhelper()
+        {
+
+            await Navigation.PushModalAsync(new Page2());
+           
+            
+        }
+        */
+      async private void btnLicz_Clicked(object sender, EventArgs e)
+        {
+           /* ThreadStart ts = new ThreadStart(Threadhelper);
+           // Console.WriteLine("Główny wątek: Tworzenie wątku pochodnego");
+            Thread thread = new Thread(ts);
+            // Uruchamiamy wątek pochodny
+           thread.Start();
+            // zatrzymanie głównego wątku
+            Thread.Sleep(2000);*/
+            
+            await Navigation.PushModalAsync(new Page3());
+            
+            await Task.Delay(100);
+        }
+
+       async private void Button_Clicked(object sender, EventArgs e)
+        {
+           await DisplayAlert("Autor", "Jan Brzyski Uniwersytet Przyrodniczy w Poznaniu","OK");
+            await Task.Delay(100);
+
+        }
+
+        /*  async private void btnLicz_Clicked(object sender, EventArgs e)
+          {
+              await Navigation.PushModalAsync(new Page1());
+          }
+
+          async private void btnAdd_Clicked(object sender, EventArgs e)
+          {
+              await Navigation.PushModalAsync(new Page2());
+          }*/
+    }
+}
